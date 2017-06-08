d8848dd917de52e197fd1db6dfd5c14dcbd39481
Elaine Kiyumi <kijapinha@gmail.com>
2017-06-08 20:19:38 -0300

    habito

---
+++ Habito/Habito/Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace Habito
+{
+    partial class Principal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblAtividade = new System.Windows.Forms.Label();
+            this.txtAtividade = new System.Windows.Forms.TextBox();
+            this.btnCadastrar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnListar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Número = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Atividade = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Alarme = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.lblRecop = new System.Windows.Forms.Label();
+            this.lblAlarme = new System.Windows.Forms.Label();
+            this.lblRecompensa = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnOk = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblAtividade
+            // 
+            this.lblAtividade.AutoSize = true;
+            this.lblAtividade.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAtividade.Location = new System.Drawing.Point(27, 79);
+            this.lblAtividade.Name = "lblAtividade";
+            this.lblAtividade.Size = new System.Drawing.Size(87, 24);
+            this.lblAtividade.TabIndex = 1;
+            this.lblAtividade.Text = "Atividade";
+            // 
+            // txtAtividade
+            // 
+            this.txtAtividade.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.txtAtividade.Location = new System.Drawing.Point(29, 105);
+            this.txtAtividade.Name = "txtAtividade";
+            this.txtAtividade.Size = new System.Drawing.Size(391, 24);
+            this.txtAtividade.TabIndex = 3;
+            // 
+            // btnCadastrar
+            // 
+            this.btnCadastrar.Location = new System.Drawing.Point(52, 295);
+            this.btnCadastrar.Name = "btnCadastrar";
+            this.btnCadastrar.Size = new System.Drawing.Size(107, 47);
+            this.btnCadastrar.TabIndex = 6;
+            this.btnCadastrar.Text = "Cadastrar";
+            this.btnCadastrar.UseVisualStyleBackColor = true;
+            this.btnCadastrar.Click += new System.EventHandler(this.btnCadastrar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(278, 295);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(107, 47);
+            this.btnCancelar.TabIndex = 7;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // btnListar
+            // 
+            this.btnListar.Location = new System.Drawing.Point(165, 295);
+            this.btnListar.Name = "btnListar";
+            this.btnListar.Size = new System.Drawing.Size(107, 47);
+            this.btnListar.TabIndex = 8;
+            this.btnListar.Text = "Listar";
+            this.btnListar.UseVisualStyleBackColor = true;
+            this.btnListar.Click += new System.EventHandler(this.btnListar_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.SteelBlue;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(432, 33);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Bons Habitos, Bons Resultados!";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Número,
+            this.Atividade,
+            this.Alarme});
+            this.dataGridView1.Location = new System.Drawing.Point(29, 83);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(411, 206);
+            this.dataGridView1.TabIndex = 13;
+            this.dataGridView1.Visible = false;
+            // 
+            // Número
+            // 
+            this.Número.HeaderText = "Número";
+            this.Número.Name = "Número";
+            // 
+            // Atividade
+            // 
+            this.Atividade.HeaderText = "Atividade";
+            this.Atividade.Name = "Atividade";
+            this.Atividade.Width = 170;
+            // 
+            // Alarme
+            // 
+            this.Alarme.HeaderText = "Alarme";
+            this.Alarme.Name = "Alarme";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label1.Location = new System.Drawing.Point(159, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(150, 31);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Você deve:";
+            this.label1.Visible = false;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker1.Location = new System.Drawing.Point(29, 163);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.ShowUpDown = true;
+            this.dateTimePicker1.Size = new System.Drawing.Size(101, 24);
+            this.dateTimePicker1.TabIndex = 11;
+            this.dateTimePicker1.Value = new System.DateTime(2017, 6, 7, 8, 0, 0, 0);
+            // 
+            // lblRecop
+            // 
+            this.lblRecop.AutoSize = true;
+            this.lblRecop.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRecop.Location = new System.Drawing.Point(165, 225);
+            this.lblRecop.Name = "lblRecop";
+            this.lblRecop.Size = new System.Drawing.Size(123, 24);
+            this.lblRecop.TabIndex = 12;
+            this.lblRecop.Text = "Recompensa";
+            this.lblRecop.Visible = false;
+            // 
+            // lblAlarme
+            // 
+            this.lblAlarme.AutoSize = true;
+            this.lblAlarme.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAlarme.Location = new System.Drawing.Point(25, 136);
+            this.lblAlarme.Name = "lblAlarme";
+            this.lblAlarme.Size = new System.Drawing.Size(70, 24);
+            this.lblAlarme.TabIndex = 0;
+            this.lblAlarme.Text = "Alarme";
+            // 
+            // lblRecompensa
+            // 
+            this.lblRecompensa.AutoSize = true;
+            this.lblRecompensa.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRecompensa.Location = new System.Drawing.Point(31, 225);
+            this.lblRecompensa.Name = "lblRecompensa";
+            this.lblRecompensa.Size = new System.Drawing.Size(128, 24);
+            this.lblRecompensa.TabIndex = 2;
+            this.lblRecompensa.Text = "Recompensa:";
+            this.lblRecompensa.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(278, 295);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 47);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Voltar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(165, 295);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(107, 47);
+            this.btnOk.TabIndex = 15;
+            this.btnOk.Text = "OK";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Visible = false;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // Principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(454, 366);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblRecompensa);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtAtividade);
+            this.Controls.Add(this.lblRecop);
+            this.Controls.Add(this.lblAlarme);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.lblAtividade);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnListar);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnCadastrar);
+            this.Name = "Principal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Principal";
+            this.Load += new System.EventHandler(this.Principal_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblAtividade;
+        private System.Windows.Forms.TextBox txtAtividade;
+        private System.Windows.Forms.Button btnCadastrar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnListar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Número;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Atividade;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Alarme;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label lblRecop;
+        private System.Windows.Forms.Label lblAlarme;
+        private System.Windows.Forms.Label lblRecompensa;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnOk;
+    }
+}
+
---
+++ Habito/Habito/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Habito
+{
+    public partial class Principal : Form
+    {
+        int atividades = 0;
+        List<string> lista = new List<string>();
+
+        public Principal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnListar_Click(object sender, EventArgs e)
+        {
+            foreach(Control c in Controls)
+            {
+                c.Visible = false;
+            }
+            button1.Visible = true;
+            label2.Visible = true;
+            dataGridView1.Visible = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (Control c in Controls)
+            {
+                c.Visible = true;
+            }
+            button1.Visible = false;
+            dataGridView1.Visible = false;
+            lblRecompensa.Visible = false;
+            lblRecop.Visible = false;
+            label1.Visible = false;
+            btnOk.Visible = false;
+        }
+
+        private void btnCadastrar_Click(object sender, EventArgs e)
+        {
+            if(txtAtividade.Text != "")
+            {
+                int i = atividades + 1;
+                dataGridView1.Rows.Insert(atividades++, i, txtAtividade.Text, dateTimePicker1.Text);
+                txtAtividade.Text = "";
+                dateTimePicker1.Text = "08:00:00";
+                timer1.Start();
+                lista.Add("Chocolate");
+                lista.Add("Sorvete");
+                lista.Add("Doce");
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            for(int j = 0; j < dataGridView1.Rows.Count - 1; j++)
+            {
+                if (dataGridView1.Rows.Count > 1)
+                {
+                    string horario = DateTime.Now.Hour + ":" + DateTime.Now.Minute;
+                    string horario_salvo = dataGridView1.Rows[j].Cells[2].Value.ToString();
+                    horario_salvo = horario_salvo.Substring(0, 5);
+                    if (horario.Equals(horario_salvo))
+                    {
+                        label1.Text = "Você deve: \n" + dataGridView1.Rows[j].Cells[1].Value.ToString();
+                        lblRecompensa.Visible = true;
+                        label1.Visible = true;
+                        lblRecop.Text = lista[j];
+                        lblRecop.Visible = true;
+                        playSound();
+
+                        txtAtividade.Visible = false;
+                        dateTimePicker1.Visible = false;
+                        btnCadastrar.Visible = false;
+                        btnListar.Visible = false;
+                        btnCancelar.Visible = false;
+                        lblAlarme.Visible = false;
+                        lblAtividade.Visible = false;
+                        btnOk.Visible = true;
+                        timer1.Stop();
+                    }
+                }
+            }
+        }
+
+        private void Principal_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.RemoveAt(0);
+            atividades--;
+            timer1.Stop();
+            foreach (Control c in Controls)
+            {
+                c.Visible = false;
+            }
+            label2.Visible = true;
+            lblAtividade.Visible = true;
+            lblAlarme.Visible = true;
+            txtAtividade.Visible = true;
+            dateTimePicker1.Visible = true;
+            btnCadastrar.Visible = true;
+            btnCancelar.Visible = true;
+            btnListar.Visible = true;
+        }
+
+        private void playSound()
+        {
+            SoundPlayer simpleSound = new SoundPlayer(@"..\..\img\Alarme_Despertador.wav");
+            simpleSound.Play();
+        }
+    }
+}
